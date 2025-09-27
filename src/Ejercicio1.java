7af6b3de396f9e6be29a68b1c7e8c3e66ee1f245
miltondw <estradamilton2001@gmail.com>
2025-09-27 07:14:11 -0500

    Ejercicio uno completado

---
+++ src/Ejercicio1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    /*
+     * ## Ejercicio 1
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Definicion de variables
+        float numberA;
+        float numberB;
+
+        System.out.println("**** Ejercicio 1 ****");
+        System.out.println("---------------------");
+
+        System.out.println("Ingresa dos numeros Reales: ");
+        // Entrada de datos
+        System.out.print("Ingresa el valor de a: ");
+        numberA = sc.nextFloat();
+        System.out.print("Ingresa el valor de b: ");
+        numberB = sc.nextFloat();
+        // Suma y Diferencia al NumberA ser mayor que el B
+        if (numberA > numberB) {
+            System.out.println("Suma: " + (numberA + numberB));
+            System.out.println("Diferencia: " + (numberA - numberB));
+        } else if (numberA <= numberB) {
+            System.out.println("Producto: " + (numberA * numberB));
+            if (numberB == 0) {
+                System.out.println("Division no definida (b es 0)");
+            } else {
+                System.out.println("División es: " + (numberA / numberB));
+            }
+        }
+        sc.close();
+    }
+}
